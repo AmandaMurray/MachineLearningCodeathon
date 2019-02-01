3857a5365ce6fb59dfa62777a09f4bc0e8c17b8e
Isabel Alvarez <iag1997@gmail.com>
2019-01-31 23:40:29 -0500

    imported csv

---
+++ ML.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_happy_data():
+    csv_path = "world-happiness-report/2016.csv"
+    return pd.read_csv(csv_path)
+
+def main():
+    happy = load_happy_data()
+    happy.head()
